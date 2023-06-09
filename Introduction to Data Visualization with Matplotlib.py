9207cea1990e8941c5d563749ff23de9082f846e
Larry <larryezego@gmail.com>
2023-06-09 16:56:29 +0100

    Intro to data viz with matplotlib

--- Introduction to Data Visualization with Matplotlib.py
+++ Introduction to Data Visualization with Matplotlib.py
@@ -787,21 +787,26 @@ fig.set_size_inches([Width, Height]) is used to control the size of the figure t
 """
 
 # Show the figure
+fig, ax = plt.subplots()
+
 plt.show()
 
 # Save as a PNG file
-fig.savefig("my_figure.png")
+fig.savefig("Introduction to Data Visualization with Matplotlib\\my_figure.png")
 
 # Save as a PNG file with 300 dpi
-fig.savefig("my_figure_300dpi.png", dpi=300)
+fig, ax = plt.subplots()
+plt.show()
+
+fig.savefig(
+    "Introduction to Data Visualization with Matplotlib\\my_figure_300dpi.png", dpi=300)
 
 # Set figure dimensions and save as a PNG
+fig, ax = plt.subplots()
 fig.set_size_inches([3, 5])
-fig.savefig("figure_3_5.png")
 
-# Set figure dimensions and save as a PNG
-fig.set_size_inches([5, 3])
-fig.savefig("figure_5_3.png")
+plt.show()
+fig.savefig("Introduction to Data Visualization with Matplotlib\\figure_3_5.png")
 
 
 """
@@ -849,4 +854,5 @@ ax.set_ylabel("Weight")
 ax.set_xticklabels(sports, rotation=90)
 
 # Save the figure to file
-fig.savefig("sports_weights.png")
+fig.savefig(
+    "Introduction to Data Visualization with Matplotlib\\sports_weights.png")
