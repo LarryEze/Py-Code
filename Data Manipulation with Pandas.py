57863a21080b0928b2fe37af8e1a8a6f2d917a66
Larry <larryezego@gmail.com>
2023-06-09 16:57:16 +0100

    Data manipulation with pandas

--- Data Manipulation with Pandas.py
+++ Data Manipulation with Pandas.py
@@ -824,12 +824,6 @@ avocados_2016[cols_with_missing].hist()
 # Show the plot
 plt.show()
 
-
-# From previous step
-cols_with_missing = ["small_sold", "large_sold", "xl_sold"]
-avocados_2016[cols_with_missing].hist()
-plt.show()
-
 # Fill in missing values with 0
 avocados_filled = avocados_2016.fillna(0)
 
